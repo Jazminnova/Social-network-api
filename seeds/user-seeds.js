67d7377d67e4f1fef19aca20a44e2c4ebcf09fe4
jazminnova <jasminnova@gmail.com>
2024-03-01 12:44:02 -0500

    added userseeds

---
+++ seeds/user-seeds.js
@@ -0,0 +1,20 @@
+const seedUsers = [
+  {
+    username: "josh",
+    email: "joshforges@gmail.com",
+  },
+  {
+    username: "Squanch",
+    email: "Squanchers0409@gmail.com",
+  },
+  {
+    username: "jacobhexa",
+    email: "jacobW@gmail.com",
+  },
+  {
+    username: "Jazz",
+    email: "Jammz@gmail.com",
+  },
+];
+
+module.exports = seedUsers;
