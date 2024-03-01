5bfb4d90cb3a370e1212e238ed5517aff8019cf0
jazminnova <jasminnova@gmail.com>
2024-03-01 12:53:02 -0500

    added thought-seed

---
+++ seeds/thought-seeds.js
@@ -0,0 +1,115 @@
+const seedThoughts = [
+  {
+    thoughtText: "This is a sample thought about something interesting.",
+    username: "josh",
+    reactions: [
+      {
+        reactionBody: "👍",
+        username: "jacobhexa",
+      },
+      {
+        reactionBody: "😄",
+        username: "squanchers",
+      },
+    ],
+  },
+  {
+    thoughtText: "This is a sample thought about something else interesting.",
+    username: "Squanchers",
+    reactions: [
+      {
+        reactionBody: "👍",
+        username: "Jazz",
+      },
+      {
+        reactionBody: "😄",
+        username: "josh",
+      },
+    ],
+  },
+  {
+    thoughtText: "This is a sample thought about something else interesting.",
+    username: "Jacobhexa",
+    reactions: [
+      {
+        reactionBody: "👍",
+        username: "jazz",
+      },
+      {
+        reactionBody: "😄",
+        username: "Squanchers",
+      },
+    ],
+  },
+  {
+    thoughtText: "This is a sample thought about something else interesting.",
+    username: "Jazz",
+    reactions: [
+      {
+        reactionBody: "👍",
+        username: "jacob",
+      },
+      {
+        reactionBody: "😄",
+        username: "josh",
+      },
+    ],
+  },
+  {
+    thoughtText: "This is a sample thought about something else interesting.",
+    username: "squanchers",
+    reactions: [
+      {
+        reactionBody: "👍",
+        username: "jazz",
+      },
+      {
+        reactionBody: "😄",
+        username: "jacobhexa",
+      },
+    ],
+  },
+  {
+    thoughtText: "This is a sample thought about something else interesting.",
+    username: "jacobhexa",
+    reactions: [
+      {
+        reactionBody: "👍",
+        username: "Squanchers",
+      },
+      {
+        reactionBody: "😄",
+        username: "jazz",
+      },
+    ],
+  },
+  {
+    thoughtText: "This is a sample thought about something else interesting.",
+    username: "josh",
+    reactions: [
+      {
+        reactionBody: "👍",
+        username: "jacobhexa",
+      },
+      {
+        reactionBody: "😄",
+        username: "jazz",
+      },
+    ],
+  },
+  {
+    thoughtText: "This is a sample thought about something else interesting.",
+    username: "jazz",
+    reactions: [
+      {
+        reactionBody: "👍",
+        username: "jacobhexa",
+      },
+      {
+        reactionBody: "😄",
+        username: "Squanchers",
+      },
+    ],
+  },
+];
+module.exports = seedThoughts;
